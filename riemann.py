2b140e6a2b22402bc067f0f32cf9bc7e10222216
mintann1701 <166739161+mintann1701@users.noreply.github.com>
2025-02-20 22:34:02 -0500

    h
    h

--- riemann.py
+++ riemann.py
@@ -5,11 +5,12 @@ def left_endpoint(x_vals: np.ndarray, func: np.ufunc) -> float:
     height = func((x_vals[:-1]))
     return np.sum(width * height)
 
-#trapezoid function
+#trapezoid
 def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
     vertices1 = np.diff(x_vals)
     vertices2 = func(x_vals)
     return np.sum((f_values[:-1] + f_values[1:]) / 2 * dx)
+
 
 #simpson function
 def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
