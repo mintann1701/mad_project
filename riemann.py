63d36e7036e8c81d43ceac941ed9235549bd35e2
mintann1701 <166739161+mintann1701@users.noreply.github.com>
2025-02-20 22:42:17 -0500

    Update riemann.py

--- riemann.py
+++ riemann.py
@@ -7,7 +7,7 @@ def left_endpoint(x_vals: np.ndarray, func: np.ufunc) -> float:
     height = func((x_vals[:-1]))
     return np.sum(width * height)
 
-#trapezoid function 
+#trapezoid function
 def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
     vertices1 = np.diff(x_vals)
     vertices2 = func(x_vals)
