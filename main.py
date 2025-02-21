c4a3c8241420add2919f5b36ff28b662c301dc77
smtran8 <davidtran.forwork@gmail.com>
2025-02-20 21:21:53 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-#Question 3:
-import matplotlib.pyplot as plt
+#Question 2 - left_endpoint function
+#left_endpoint function
 import numpy as np
+def left_endpoint(x_vals: np.ndarray, func: np.ufunc) -> float:
+    width = np.diff(x_vals)
+    height = func((x_vals[:-1]))
+    return np.sum(width * height)
+
+#simpson function
+def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
+    a = x_vals[0]
+    b = x_vals[-1]
+    mid_point = (a + b) / 2
+    calculate_integral = ((b-a)/6) * (func(a) + func(b) + 4*func(mid_point))
+    return calculate_integral
+
